e4eb7f3f37b20ab7e7d0b808d7c9b5a5e8036efe
joaocarl0s <joaocarlos.mazevedo12@gmail.com>
2021-07-29 13:40:20 -0300

    funcoes para iniciar e prosseguir o jogo criadas.

--- app.py
+++ app.py
@@ -7,6 +7,29 @@ def selecionarPalavra():
     palavra = random.choice(palavras)
     return palavra.upper()
 
+#Iniciando jogo.
+def jogar(palavra):
+    #Definindo variáveis
+    palavra_incompleta = "_" * len(palavra)
+    advinhou = False
+    letras_utilizadas = []
+    palavras_utilizadas = []
+    tentativas = 6
+
+    print(palavra)
+    print(palavra_incompleta)
+
+    #Boas vindas
+    print("Começando Jogo.")
+    print(exibirForca(tentativas))
+    print("Esta é a palavra: ", palavra_incompleta)
+
+    #While o usuário não adivinhar e houver tentativas.
+
+    while not advinhou and tentativas > 0:
+        palpite = input("Digite uma palavra ou letra para continuar: ").upper()
+        print(palpite)
+
 #Status do jogo
 def exibirForca(tentativas):
     estagios = [# Fim de jogo
@@ -79,9 +102,13 @@ def exibirForca(tentativas):
                   |     
                   -
               """
-  ]
+    ]
 
     return estagios[tentativas]
 
-test = selecionarPalavra()
-print(test)
+#Iniciação do jogo e validação para continuar.
+def inicioJogo():
+    palavra = selecionarPalavra()
+    jogar(palavra)
+
+inicioJogo()
